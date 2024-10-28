7ac02292a911ae66e787ab5e53028a94cae345d5
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-10-28 15:03:40 +0530

    POST Request in Express

--- server.js
+++ server.js
@@ -16,8 +16,28 @@ const friends = [
 ];
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+  const start = Date.now();
   next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: 'Missing friend name',
+    });
+  }
+
+  const newFriend = {
+    id: friends.length,
+    name: req.body.name,
+  };
+  friends.push(newFriend);
+
+  res.json(newFriend);
 });
 
 app.get('/friends', (req, res) => {
