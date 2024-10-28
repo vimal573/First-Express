2cf89a823033ccdbc460bc702725d1e597924158
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-10-28 10:30:55 +0530

    Route parameters

--- server.js
+++ server.js
@@ -4,8 +4,33 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+const friends = [
+  {
+    id: 0,
+    name: 'albert Einstein',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:id', (req, res) => {
+  const friendId = Number(req.params.id);
+  const friend = friends[friendId];
+
+  // GET /friends/22
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: 'Friend does not exist',
+    });
+  }
 });
 
 app.get('/messages', (req, res) => {
