377568a36ab4519758c79004aefe7b529068ad30
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-10-28 11:06:25 +0530

    Writing your Own middileware

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/friends', (req, res) => {
   res.json(friends);
 });
